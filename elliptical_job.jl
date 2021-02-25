092779bbba372a830b2f6ebddf9c14c4e37ce99a
Anthony Polloreno <ampolloreno@gmail.com>
2021-02-25 11:40:55 -0700

    foo

--- elliptical_job.jl
+++ elliptical_job.jl
@@ -7,7 +7,7 @@ using DelimitedFiles
 
 σ1 = .1
 σ2 = 1
-amp = .01
+amp = .001
 
 start = time()
 println(start)
@@ -62,7 +62,7 @@ end
 Γ = 1/62
 ω = 2*π*180E3
 θ = -π/2;
-max_order = 200
+max_order = 20
 b = SpinBasis(1//2)
 ψ0 = 1/sqrt(2) * (spindown(b) + spinup(b))
 U = 2 * π * 10E3

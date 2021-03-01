d501781546e78e8f8d8ea972a1258c578fccbc04
Anthony Polloreno <ampolloreno@gmail.com>
2021-03-01 16:10:25 -0700

    fooush

--- elliptical_job.jl
+++ elliptical_job.jl
@@ -7,7 +7,7 @@ using DelimitedFiles
 
 σ1 = .2
 σ2 = .1
-amp = .0001
+amp = .001
 
 start = time()
 println(start)
@@ -72,7 +72,7 @@ end
 max_order = 20 
 b = SpinBasis(1//2)
 ψ0 = 1/sqrt(2) * (spindown(b) + spinup(b))
-U = 2 * π * 10E3
+U = 2 * π * 10E4
 evolution_time = π/(2*U*amp)
 step_size = evolution_time/1
 T = [0.0:step_size:evolution_time;];

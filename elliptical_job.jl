af5bcede6ada9627e6a4ebe05e4ec0de114c77b2
Anthony Polloreno <ampolloreno@gmail.com>
2021-03-01 16:39:44 -0700

    fooush

--- elliptical_job.jl
+++ elliptical_job.jl
@@ -69,10 +69,10 @@ end
 Γ = 1/62
 ω = 2*π*180E3
 θ = -π/2;
-max_order = 30 
+max_order = 40 
 b = SpinBasis(1//2)
 ψ0 = 1/sqrt(2) * (spindown(b) + spinup(b))
-U = 2 * π * 10E5
+U = 2 * π * 10E3
 evolution_time = π/(2*U*amp)
 step_size = evolution_time/1
 T = [0.0:step_size:evolution_time;];

9d4261bafe4c56a81e4438c61d045cee8a510bcd
Anthony Polloreno <ampolloreno@gmail.com>
2021-03-12 09:06:04 -0700

    foo

--- elliptical_job.jl
+++ elliptical_job.jl
@@ -6,8 +6,8 @@ using DelimitedFiles
 
 
 σ1 = .1
-σ2 = .2 
-amp = .1 #bringing this up to .1 fixed it, I have no idea why...
+σ2 = 1  
+amp= .1 #bringing this up to .1 fixed it, I have no idea why...
 
 start = time()
 println(start)

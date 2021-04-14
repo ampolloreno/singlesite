4660e556060e53077a4bc71b9632e102adb55eab
Anthony Polloreno <ampolloreno@gmail.com>
2021-04-14 09:21:46 -0600

    increased U, lets see

--- elliptical_job.jl
+++ elliptical_job.jl
@@ -125,12 +125,12 @@ max_order = 15
 data = hcat([[c[1] for c in [cond_eval(n, m) for n in range(0, maxn, step=1)]] for m in range(0, max_order, step=1)]...)
 
 
-ω = 2*π*180E3
+ω = 2*π*180E4
 θ = -π/2;
 # From numerical experiments it seems like 40 is sufficient to match the pattern for .1, 1., to an accuracy of .003.
 b = SpinBasis(1//2)
 ψ0 = 1/sqrt(2) * (spindown(b) + spinup(b))
-U = BigFloat(2 * π * 10E3)
+U = BigFloat(2 * π * 10E4) #bringing this up
 evolution_time = π/(2*U*amp)
 step_size = evolution_time/1
 T = [0.0:step_size:evolution_time;];

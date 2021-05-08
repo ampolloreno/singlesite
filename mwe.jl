6bc4caefd9d8d4b2f1c585748e16ce0a67ac4c96
Anthony Polloreno <ampolloreno@gmail.com>
2021-05-08 10:26:42 -0600

    elliptical gauss with parallel dispalced to see if it works

--- mwe.jl
+++ mwe.jl
@@ -78,7 +78,7 @@ function sequential_exact_evolution_evaluator_factory(ψ0, T, maxm, U, θ, ω, b
                 total = 0
                 H_odf(ρ, ϕ, t, 0, U, θ, order1, ω)*sigmaz(b)
             end
-            _, ψ = timeevolution.schroedinger_dynamic(T, ψ, H; maxiters=1e7,  alg=OrdinaryDiffEq.Rodas4P(autodiff=false))
+            _, ψ = timeevolution.schroedinger_dynamic(T, ψ, H; maxiters=1e9,  alg=OrdinaryDiffEq.Rodas4P(autodiff=false))
             ψ = last(ψ)
         end
         ψ

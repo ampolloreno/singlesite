9ead2bc3c9f2aa5eed61a2e2d3d0284e48f5df2c
Anthony Polloreno <ampolloreno@gmail.com>
2021-05-08 22:35:28 -0600

    elliptical gauss with parallel dispalced to see if it works

--- mwe2.jl
+++ mwe2.jl
@@ -106,7 +106,7 @@ function infidelity_across_disk(F1, F2)
     end
 end
 
-function timeevolve(evolution_time, ψ, H; step=10E-8) # Assume H is proportional to Z
+function timeevolve(evolution_time, ψ, H; step=10E-) # Assume H is proportional to Z
     T = [0.0:step:evolution_time;]
     a = 1
     b = 1

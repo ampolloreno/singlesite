055778d36c12c5cb6f7bddaf14ed8f680f37c284
Anthony Polloreno <ampolloreno@gmail.com>
2021-05-09 11:46:48 -0600

    elliptical gauss with parallel dispalced to see if it works

--- elliptical_job_solver.jl
+++ elliptical_job_solver.jl
@@ -113,7 +113,7 @@ function sequential_exact_evolution_evaluator_factory(ψ0, T, maxm, U, θ, ω, b
         for order1 in orders
             for order2 in range(0, maxn, step=1)
                 H(t, _) = H_odf(ρ, ϕ, t, 0, U, θ, order1, order2, ω)*sigmaz(b)
-                _, ψ = timeevolution.schroedinger_dynamic(T, ψ, H; alg=OrdinaryDiffEq.ROCK())# dtmin=1e-3)#; dtmin=1e-5, dt=1.1e-4)#;maxiters=1e5)# abstol=1e-10, reltol=1e-8)
+                _, ψ = timeevolution.schroedinger_dynamic(T, ψ, H; alg=OrdinaryDiffEq.ROCK4())# dtmin=1e-3)#; dtmin=1e-5, dt=1.1e-4)#;maxiters=1e5)# abstol=1e-10, reltol=1e-8)
                 ψ = last(ψ)
             end
         end
